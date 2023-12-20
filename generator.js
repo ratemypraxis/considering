b80e90e03b947c4e1b86725fe5b8de7348be908a
Sam <49932341+ratemypraxis@users.noreply.github.com>
2023-12-20 14:05:24 -0500

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,60 +1,61 @@
+// caling filesystem and parts of speech js libraries
 const fs = require('fs');
 const pos = require('pos');
 
-// Read the contents of wellness.txt and security.txt
+// reading in source text files
 const wellnessContent = fs.readFileSync('wellness.txt', 'utf-8');
 const securityContent = fs.readFileSync('security.txt', 'utf-8');
 
-// Split the contents into an array of sentences
+// breaking texts into sentences
 const wellnessSentences = wellnessContent.split('\n').filter(Boolean);
 const securitySentences = securityContent.split('\n').filter(Boolean);
 
-// Generate a combined sentence
+// re-building into new sentences from following function rules
 const combinedSentence = generateCombinedSentence(wellnessSentences, securitySentences);
 
 console.log(combinedSentence);
 
-// Function to generate a combined sentence
+// generate new sentences
 function generateCombinedSentence(wellnessSentences, securitySentences) {
   const combinedTokens = [];
 
-  // Iterate over the sentences
+  // adding variation to the new sentences -- avoid getting the same thing over and over
   const minLength = Math.min(wellnessSentences.length, securitySentences.length);
   for (let i = 0; i < minLength; i++) {
     const wellnessSentence = wellnessSentences[i];
     const securitySentence = securitySentences[i];
 
-    // Tokenize and tag the sentences
+    // tag word types in source sentences
     const wellnessTagged = tagSentence(wellnessSentence);
     const securityTagged = tagSentence(securitySentence);
 
-    // Find appropriate words to construct the sentence
+    // grab specific word types from difference source sentence arrays
     const verb = getRandomWordByTag(wellnessTagged, 'VB');
     const adjective = getRandomWordByTag(securityTagged, 'JJ');
     const noun = getRandomWordByTag(securityTagged, 'NN');
 
-    // Check if all words are filled
+    // make sure we have each word type represented
     if (verb && adjective && noun) {
-      // Build the sentence
+      // put it all together into a new sentence
       const sentence = `Consider ${verb}ing like an ${adjective} ${noun}.`;
       combinedTokens.push(sentence);
     }
   }
 
-  // Randomly select a single sentence from the available options
+  // randomly choose a single generated sentence
   const randomIndex = Math.floor(Math.random() * combinedTokens.length);
   const combinedSentence = combinedTokens[randomIndex];
   return combinedSentence;
 }
 
-// Function to tokenize and tag a sentence
+// pos sentence tagging 
 function tagSentence(sentence) {
   const words = new pos.Lexer().lex(sentence);
   const tagger = new pos.Tagger();
   return tagger.tag(words);
 }
 
-// Function to get a random word based on a specific tag, with fallback options
+// pos word grabbing by tag
 function getRandomWordByTag(taggedWords, tag) {
   const wordsWithMatchingTag = taggedWords.filter((word) => word[1].startsWith(tag));
   if (wordsWithMatchingTag.length === 0) {
@@ -64,10 +65,10 @@ function getRandomWordByTag(taggedWords, tag) {
   return wordsWithMatchingTag[randomIndex][0];
 }
 
-// Function to get a random word from the tagged words
+// random selected of word by tag
 function getRandomWord(taggedWords) {
   if (taggedWords.length === 0) {
-    return ''; // Return an empty string if no tagged words are available
+    return ''; // blanks if no returned word
   }
   const randomIndex = Math.floor(Math.random() * taggedWords.length);
   return taggedWords[randomIndex][0];
